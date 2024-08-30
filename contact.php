bb397ad9c6948d3495a35c7cce172d0c11480ac4
NoursInDev <nours44.mc@gmail.com>
2024-08-30 14:03:11 +0200

    Code - Patch Transfer PHP500

--- contact.php
+++ contact.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Content-Type: text/html; charset=UTF-8");
 $svname = "localhost";
 $user = "root";
@@ -9,27 +13,31 @@ $dbname = "portfolio";
 $conn = mysqli_connect($svname, $user, $pass, $dbname);
 
 // check connection
-if(!$conn){
-    die("Connection Failed: ".mysqli_connect_error());
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // instruction prepare permet de préparer la requête
-$stmt = $conn->prepare("INSERT INTO contact (name, email, society, message) VALUES (?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO contact (name, email, society, message) VALUES (?, ?, ?, ?)");
 
 // bind_param permet de lier les paramètres à la requête, ssss signifie que les paramètres sont de type string
-$stmt->bind_param("ssss", $name, $email, $society, $message);
+    $stmt->bind_param("ssss", $name, $email, $society, $message);
 
 // récupération des données du formulaire
-$name = $_POST['name'];
-$email = $_POST['email'];
-$society = $_POST['society'];
-$message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $society = $_POST['society'];
+    $message = $_POST['message'];
 
 // exécution de la requête
-$stmt->execute();
+    if (!$stmt->execute()) {
+        die("Erreur d'exécution de la requête: " . $stmt->error);
+    }
+
+    echo "Nouvel enregistrement créé avec succès";
 
 // fermeture de la requête
-$stmt->close();
+    $stmt->close();
 }
 $conn->close();
 
